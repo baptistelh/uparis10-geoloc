c9a3975df9a540e525e83a780ea73629b11c364f
baptiste.lehouarner@gmail.com <baptiste.lehouarner@gmail.com@85ff5fdc-da0d-dd34-9e42-d99dc92f99ef>
2013-02-09 15:39:42 +0000

    Suppression des tests

--- hello!/app/controllers/Application.java
+++ hello!/app/controllers/Application.java
@@ -8,12 +8,13 @@ import java.util.*;
 import models.*;
 
 public class Application extends Controller {
-    /*
     public static void index() {
         render();
-    }*/
-    public static void index() {
-	Utilisateur u = Utilisateur.find("byPseudo","daVay").first();
-	render(u);
     }
+	/*
+    public static void index() {
+    	Utilisateur u = Utilisateur.find("byPseudo","daVay").first();
+    	render(u);
+    }
+    */
 }
