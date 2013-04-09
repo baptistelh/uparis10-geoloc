76d2ac314b0a533766fed872da75ec63fc3a300a
flegaludec@gmail.com <flegaludec@gmail.com@85ff5fdc-da0d-dd34-9e42-d99dc92f99ef>
2013-04-09 08:17:45 +0000

    Implementation des beans

--- trunk/hello!/app/models/Batiment.java
+++ trunk/hello!/app/models/Batiment.java
@@ -1,5 +1,24 @@
 package models;
 
-public class Batiment {
+import java.util.*;
+import javax.persistence.*;
+import play.db.jpa.*;
 
+@Entity
+public class Batiment extends Model {
+	public String nom_bat;
+	public double longitude;
+	public double latitude;
+	public int surface;
+	public int capacite;
+	
+	public Batiment(String nom_bat, double longitude, double latitude,
+			int surface, int capacite) {
+		this.nom_bat = nom_bat;
+		this.longitude = longitude;
+		this.latitude = latitude;
+		this.surface = surface;
+		this.capacite = capacite;
+	}
+	
 }
--- trunk/hello!/app/models/Utilisateur.java
+++ trunk/hello!/app/models/Utilisateur.java
@@ -2,7 +2,6 @@ package models;
  
 import java.util.*;
 import javax.persistence.*;
- 
 import play.db.jpa.*;
  
 @Entity
