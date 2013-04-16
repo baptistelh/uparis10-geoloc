8c175966f26fe1ef4514e38929cbf70a677970e4
flegaludec@gmail.com <flegaludec@gmail.com@85ff5fdc-da0d-dd34-9e42-d99dc92f99ef>
2013-04-16 13:07:10 +0000

    oubli du @Test

--- hello!/test/TestActivite.java
+++ hello!/test/TestActivite.java
@@ -6,7 +6,7 @@ import models.*;
 public class TestActivite extends UnitTest {
 	Activite a = new Activite("miage");
 
-    
+    @Test
     public void nameNotNullTest() {
     	assertNotNull(a.description);
     }
