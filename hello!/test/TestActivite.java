555f5ca6ab070b10c02c0d4a51f8523495eed8a3
flegaludec@gmail.com <flegaludec@gmail.com@85ff5fdc-da0d-dd34-9e42-d99dc92f99ef>
2013-04-16 13:02:40 +0000

    modification du test unitaire activite (car ne contient plus la reference a batiment)

--- hello!/test/TestActivite.java
+++ hello!/test/TestActivite.java
@@ -4,13 +4,8 @@ import play.test.*;
 import models.*;
 
 public class TestActivite extends UnitTest {
-	Batiment b = new Batiment("G", 1.203, 1.110, 32, 300);
-	Activite a = new Activite(b,"miage");
+	Activite a = new Activite("miage");
 
-    @Test
-    public void containsBatimentTest() {
-    	assertNotNull(a.batiment);
-    }
     
     public void nameNotNullTest() {
     	assertNotNull(a.description);
