0d18f9f37b93ed0e417bb5e07c7aa7a4cc0f0686
onur.mia@gmail.com <onur.mia@gmail.com@85ff5fdc-da0d-dd34-9e42-d99dc92f99ef>
2013-04-19 11:34:03 +0000

    ajout du test unitaire batiment_activité

---
+++ hello!/test/Batiment_ActiviteTest.java
@@ -0,0 +1,46 @@
+
+
+import static org.junit.Assert.*;
+
+import models.Activite;
+import models.Batiment;
+import models.Batiment_Activite;
+import models.Description;
+
+import org.junit.Test;
+
+public class Batiment_ActiviteTest {
+
+	public void batimentNull(){
+		Batiment b=new Batiment("batiment G", 2, 48.3);
+		Activite a = new Activite("miage");
+		Batiment_Activite ba= new Batiment_Activite(b, a);
+		assertNotNull(ba.batiment);	
+	}
+	
+	public void activiteNull(){
+		Batiment b=new Batiment("batiment G", 2, 48.3);
+		Activite a = new Activite("miage");
+		Batiment_Activite ba= new Batiment_Activite(b, a);
+		assertNotNull(ba.activite);	
+	}
+	
+	public void descriptionsNull(){
+		Batiment b=new Batiment("batiment G", 2, 48.3);
+		Activite a = new Activite("miage");
+		Batiment_Activite ba= new Batiment_Activite(b, a);
+		Description d= new Description("date de creation", "1936",ba);
+		assertNotNull(ba.descriptions);	
+	}
+	
+	@Test
+	public void testBatiment_Activite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
