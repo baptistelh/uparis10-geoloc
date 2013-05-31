dcd636a41106cabee541afb1e78886d5bea91fcd
dav.vay@gmail.com <dav.vay@gmail.com@85ff5fdc-da0d-dd34-9e42-d99dc92f99ef>
2013-05-31 14:34:36 +0000

    - MAJ du fichier BatimentActiviteTest

--- trunk/hello!/test/BatimentActiviteTest.java
+++ trunk/hello!/test/BatimentActiviteTest.java
@@ -1,51 +1,79 @@
-
-
 import static org.junit.Assert.*;
+
+import java.util.List;
 
 import models.Activite;
 import models.Batiment;
 import models.BatimentActivite;
 import models.Description;
+import models.Utilisateur;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import play.test.Fixtures;
 import play.test.UnitTest;
 
 public class BatimentActiviteTest extends UnitTest {
 
+	Batiment b = new Batiment("batiment G", 2, 48.3);
+	Activite a = new Activite("miage");
+	BatimentActivite ba = new BatimentActivite(b, a);
+	
+	@Before
+	public void setUp() {
+		Fixtures.loadModels("data.yml");
+	}
+
+	@After
+	public void tearDown() {
+		Fixtures.deleteAllModels();
+	}
+
 	@Test
-	public void batimentNull(){
-		Batiment b=new Batiment("batiment G", 2, 48.3);
-		Activite a = new Activite("miage");
-		BatimentActivite ba= new BatimentActivite(b, a);
-		assertNotNull(ba.batiment);	
+	public void batimentNull() {
+
+		assertNotNull(ba.batiment);
+		assertNotNull(ba.activite);
+		assertNotNull(ba.descriptions);
+		assertEquals(1, ba.batiment.activites.size());
+		assertEquals(1, ba.activite.batiments.size());
+
+		assertEquals("miage", ba.batiment.activites.get(0).activite.libelle);
+		assertEquals("batiment G", ba.batiment.activites.get(0).batiment.nom);
+
+		assertEquals("miage", ba.activite.batiments.get(0).activite.libelle);
+		assertEquals("batiment G", ba.activite.batiments.get(0).batiment.nom);
+
+	}
+
+	@Test
+	public void testToString() {
+		assertEquals("batiment G miage", ba.toString());
+
 	}
 	
 	@Test
-	public void activiteNull(){
-		Batiment b=new Batiment("batiment G", 2, 48.3);
-		Activite a = new Activite("miage");
-		BatimentActivite ba= new BatimentActivite(b, a);
-		assertNotNull(ba.activite);	
+	public void testUserFields() {
+
+		BatimentActivite batAct = BatimentActivite.all().first();
+		assertEquals( "Sport", batAct.activite.libelle);
+		assertEquals( "batA", batAct.batiment.nom);
+		assertTrue( 50.4d == batAct.batiment.latitude);
+
 	}
-	
+
 	@Test
-	public void descriptionsNull(){
-		Batiment b=new Batiment("batiment G", 2, 48.3);
-		Activite a = new Activite("miage");
-		BatimentActivite ba= new BatimentActivite(b, a);
-		//Description d= new Description("date de creation", "1936",ba);
-		assertNotNull(ba.descriptions);	
+	public void testUsersFields() {
+
+		List<BatimentActivite> batAct = BatimentActivite.all().fetch();
+		assertEquals(4, batAct.size());
+		assertTrue(50.4d == batAct.get(0).batiment.latitude);
+		assertTrue(54.5d == batAct.get(1).batiment.longitude);
+		assertEquals("Sport", batAct.get(2).activite.libelle);
+		assertEquals("batC", batAct.get(3).batiment.nom);
+		
 	}
-	
-//	@Test
-//	public void testBatiment_Activite() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testToString() {
-//		fail("Not yet implemented");
-//	}
 
 }
