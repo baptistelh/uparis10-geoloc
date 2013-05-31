bd3b04bbf418d3dcdb58c05f0dc47c7946caea36
dav.vay@gmail.com <dav.vay@gmail.com@85ff5fdc-da0d-dd34-9e42-d99dc92f99ef>
2013-05-31 14:33:32 +0000

    - MAJ du fichier UtilisateurTest

--- hello!/test/UtilisateurTest.java
+++ hello!/test/UtilisateurTest.java
@@ -3,6 +3,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.*;
+
 import play.test.*;
 import models.*;
 
@@ -13,26 +14,49 @@ public class UtilisateurTest extends UnitTest {
 	@Before
 	public void setUp(){
 		u = new Utilisateur("pseudo", "mdp", "mail");	
+		Fixtures.loadModels("data.yml");
 	}
 	
 	@After
 	public void tearDown(){
 		u = null;
+		Fixtures.deleteAllModels();
 	}
 	
 	@Test
-	public void pseudoUtilisateur() {
+	public void testLogin() {
 		assertEquals("pseudo", u.pseudo);
 	}
 
 	@Test
-	public void mdpUtilisateur() {
+	public void testPwd() {
 		assertEquals("mdp", u.mdp);
 	}
 
 	@Test
-	public void capaciteBatiment() {
+	public void testMail() {
 		assertEquals("mail", u.mail);
 	}
+	
+	@Test
+	public void testUserFields() {
+
+		Utilisateur user = Utilisateur.all().first();
+		assertEquals( "davay@gmail.com", user.mail);
+		assertEquals( "secret", user.mdp);
+		assertEquals( "daVay", user.pseudo);
+
+	}
+
+	@Test
+	public void testUsersFields() {
+
+		List<Utilisateur> users = Utilisateur.all().fetch();
+		assertEquals(2, users.size());
+		assertEquals("davay@gmail.com", users.get(0).mail);
+		assertEquals("azel", users.get(1).pseudo);
+
+	}
+
 
 }
