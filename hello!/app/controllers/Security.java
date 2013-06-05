f55d0eed4fb619b78e01bfb90995c741537ef311
diakite.mohamed001@gmail.com <diakite.mohamed001@gmail.com@85ff5fdc-da0d-dd34-9e42-d99dc92f99ef>
2013-06-05 22:46:22 +0000

    Rajout de tests fonctionnels + tests Selenium

--- hello!/app/controllers/Security.java
+++ hello!/app/controllers/Security.java
@@ -3,17 +3,12 @@ package controllers;
 import models.*;
 
 public class Security extends Secure.Security {
-	
-//    static boolean authenticate(String username, String password) {
-//    	if(username.equals("toto") && password.equals("toto"))
-//    		return true;
-//    	else
-//    		return false;
-//    }
-    
+	    
     static boolean authenticate(String username, String password) {
     	Utilisateur u = Utilisateur.find("byPseudo", username).first();
-    	if(u!=null && password.equals(u.mdp)) return true;
+    	if(u!=null){
+    		return password.equals(u.mdp);
+    	}
     	else return false;
     }
     
--- hello!/test/ApplicationTest.java
+++ hello!/test/ApplicationTest.java
@@ -70,7 +70,6 @@ public class ApplicationTest extends FunctionalTest {
         
         //On effectue un autre test pour la non-authentification
         userParam.put("username", userParam.get("username").concat("_False"));
-        System.out.println(userParam);
         loginResponse = POST("/secure/authenticate", userParam);
         assertTrue(loginResponse.getHeader("Location").equals("/secure/login"));
         
@@ -78,14 +77,86 @@ public class ApplicationTest extends FunctionalTest {
     
     
     // Tests des webServices
-   /* @Test
-    public void batimentById(){
-    	Batiment batTrue= new Batiment("batA", 54.5, 50.4);
-    	Batiment batFalse= new Batiment("batA", 54.5, 50.4);
-    	//Application.batimentById(1);
-    	Response r = GET ("/listeBatimentsXML");
-    	System.out.println(r.toString());
+    
+    /**
+     * On teste l'accessibilité de la page d'accueil de la partie WebServices
+     */
+    @Test
+    public void testThatWebServicesPageWorks() {
+    	Response response = GET("/webservices");
+        assertIsOk(response);
+        assertContentType("text/html", response);
+        assertCharset(play.Play.defaultWebEncoding, response);
+    }
+    
+    /**
+     * Teste si les webservices dediés au XML renvoient les données au format XML
+     */
+    @Test
+    public void testWebServicesXML() {
+    	Response response = GET("/webservices/listeBatimentsXML");
+    	assertIsOk(response);
+        assertContentType("text/xml", response);
+        assertCharset(play.Play.defaultWebEncoding, response);
+        
+        response = GET("/webservices/listeBatimentsIdXML");
+    	assertIsOk(response);
+        assertContentType("text/xml", response);
+        assertCharset(play.Play.defaultWebEncoding, response);
+    }
+    
+    /**
+     * Teste si les webservices dediés au JSON renvoient les données au format JSON
+     */
+    @Test
+    public void testWebServicesJSON() {
+    	Response response = GET("/webservices/listeBatimentsJSON");
+    	assertIsOk(response);
+        assertContentType("application/json", response);
+        
+        response = GET("/webservices/listeBatimentsIdJSON");
+    	assertIsOk(response);
+        assertContentType("application/json", response);
+    }
+    
+    /**
+     * Teste si la page de récupération du batiment à partir de l'id en XML renvoie au format XML
+     */
+    @Test
+    public void testGetBatimentByIdXML() {
+    	Map<String, String> userParam = new HashMap<String, String>();
+    	Batiment b = Batiment.all().first();
+    	Long id = b.id;;
+    	userParam.put("id", String.valueOf(id));
+    	Response response = POST("/application/batXML", userParam);
+    	
+    	//On teste la redirection
+    	assertStatus(302, response);
+        assertHeaderEquals("Location", "/webservices/batimentXML/"+id, response);
+    	
+        Response newResponse = GET("/webservices/batimentXML/"+id);
+        assertIsOk(newResponse);
+        assertContentType("text/xml", newResponse);
+        assertCharset(play.Play.defaultWebEncoding, newResponse);
+    }
 
-   
-    }*/
+    /**
+     * Teste si la page de récupération du batiment à partir de l'id en JSON renvoie au format JSON
+     */
+    @Test
+    public void testGetBatimentByIdJSON() {
+    	Map<String, String> userParam = new HashMap<String, String>();
+    	Batiment b = Batiment.all().first();
+    	Long id = b.id;;
+    	userParam.put("id", String.valueOf(id));
+    	Response response = POST("/application/batJSON", userParam);
+    	
+    	//On teste la redirection
+    	assertStatus(302, response);
+        assertHeaderEquals("Location", "/webservices/batimentJSON/"+id, response);
+    	
+        Response newResponse = GET("/webservices/batimentJSON/"+id);
+        assertIsOk(newResponse);
+        assertContentType("application/json", newResponse);
+    }
 }
